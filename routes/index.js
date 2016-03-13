fe4a614cc3a1d9b06ef9907d3a20058fed721258
Daniel Andrei Minca <mandrei17@gmail.com>
2016-03-13 17:39:04 +0200

    Configure MongoDB and add first record
    Signed-off-by: Daniel Andrei Minca <mandrei17@gmail.com>

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var mongodb = require('mongodb');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/thelist', function(req, res){
+  var MongoClient = mongodb.MongoClient;
+
+  var url = 'mongodb://localhost:27017/nomon';
+
+  MongoClient.connect(url, function(err, db){
+    if(err) {
+      console.log('Unable to connect to the server', err);
+    } else {
+      console.log('Connection established!');
+
+      var collection = db.collection('students');
+
+      collection.find({}).toArray(function(err,result){
+        if(err) {
+          res.send(err);
+        } else if (result.length){
+          res.render('studentlist', {
+            "studentlist": result
+          });
+        } else {
+          res.send('No documents found');
+        }
+
+        db.close();
+      });
+    }
+  })
 });
 
 module.exports = router;
